227c244940b76702e0970c13110a7005dba42c3d
deepwubs <pchucklambs@gmail.com>
2023-06-27 21:21:18 -0500

    a commit

--- robowave-2023/assets/js/main.js
+++ robowave-2023/assets/js/main.js
@@ -1,1 +1,32 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+    // fullpagejs
+    $('#fullpage').fullpage({
+        //options here
+        licenseKey: 'gplv3-license',
+        navigation: true,
+        navigationTooltips: ['Welcome', 'New Robots', 'Used Robots', 'Contact']
+    });
+    // typewriter
+    var typewriterElements = document.querySelectorAll('.typewriter-effect')
+
+    for (var i = 0; i < typewriterElements.length; i++) {
+        var currentEl = typewriterElements[i]
+        var innerText = currentEl.innerText
+       
+        new Typewriter(currentEl, {
+            loop: true,
+        })
+        .typeString(innerText)
+        .pauseFor(1000)
+        .start()
+    }
+
+    // powerglitch
+    PowerGlitch.glitch('.glitch', {
+        hideOverflow: true
+    })
+
+});
+
+
+
